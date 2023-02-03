0494dbd63bf0ccc59e9ebdf01d23d9e61d585161
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-02-03 22:18:57 +0300

    feat: add .env handle, add validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { DbModule } from './user/db/db.module';
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DbModule, UserModule],
+  imports: [DbModule, UserModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = parseInt(process.env.PORT, 10) || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT);
 }
 bootstrap();
