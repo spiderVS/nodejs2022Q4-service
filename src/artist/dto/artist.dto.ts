6dca41b8d258b80d11b37dc0d39caa82d4da7859
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-02-04 21:22:48 +0300

    fix: artist dto

--- src/artist/dto/artist.dto.ts
+++ src/artist/dto/artist.dto.ts
@@ -1,13 +1,16 @@
-import { IsBoolean, IsString, IsUUID } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { Artist } from '../interfaces/artist.interface';
 
 export class ArtistDTO implements Artist {
+  @IsNotEmpty()
   @IsUUID()
   readonly id: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly name: string;
 
+  @IsNotEmpty()
   @IsBoolean()
   readonly grammy: boolean;
 }
